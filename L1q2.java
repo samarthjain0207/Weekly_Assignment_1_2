8bc4f1b8c407295f4a31accf0a261232b130e19b
samarthjain0207 <samarthjain7020@gmail.com>
2026-03-25 13:54:52 +0530

    Level 1 Q3

--- L1q2.java
+++
@@ -1,7 +0,0 @@
-public class L1q2 {
-	public static void main(String[] args) {
-		int p=95,c=96,m=94;
-		double avg=(p+c+m)/3;
-		System.out.println("Sam's average mark in PCM is "+avg);
-	}
-}
---
+++ L1q3.java
@@ -0,0 +1,7 @@
+public class L1q3 {
+	public static void main(String[] args) {
+		double km=10.8,mile;
+		mile=10.8*1.6;
+		System.out.println("The distance " +km+ "km in miles is "+mile);
+	}
+}
