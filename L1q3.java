625a41a812cb391b6a62d2bff8563fb3d11fd42a
samarthjain0207 <samarthjain7020@gmail.com>
2026-03-25 14:04:55 +0530

    Level 1 Q4

--- L1q3.java
+++
@@ -1,7 +0,0 @@
-public class L1q3 {
-	public static void main(String[] args) {
-		double km=10.8,mile;
-		mile=10.8*1.6;
-		System.out.println("The distance " +km+ "km in miles is "+mile);
-	}
-}
---
+++ L1q4.java
@@ -0,0 +1,9 @@
+public class L1q4 {
+	public static void main(String[] args) {
+		double selling_price=191,cost_price=129;
+		double profit = selling_price-cost_price;
+		double profit_pr=profit/cost_price*100;
+		System.out.println("The Cost Price is INR "+cost_price+" and Selling Price is INR "+selling_price);
+		System.out.println("The Profit is INR "+profit+" and the Profit Percentage is "+profit_pr);
+	}
+}
