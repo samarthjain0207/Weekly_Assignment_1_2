60bff2948314773b6df100d6a1e871086be9abd7
samarthjain0207 <samarthjain7020@gmail.com>
2026-03-25 12:57:45 +0530

    Main

---
+++ L1q1.java
@@ -0,0 +1,8 @@
+public class L1q1 {
+	public static void main(String[] args) {
+
+		int year = 2024, bdyear = 2000;
+		int age=year-bdyear;
+		System.out.println("Harry's Age in "+year+" is "+age);
+	}
+}
